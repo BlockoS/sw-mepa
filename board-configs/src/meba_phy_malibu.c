edc0394e8b7fbbca0b5d396fd23e0fa26130a506
Praveen Kumar S <Praveenkumar.S@microchip.com>
2025-08-08 13:09:09 +0530

    MEPA-1034 M10G Link drop issue due to LOPC

--- board-configs/src/meba_phy_malibu.c
+++ board-configs/src/meba_phy_malibu.c
@@ -163,4 +163,12 @@ void m10g_mode_conf(const vtss_inst_t inst, meba_inst_t meba_inst, mepa_port_no_
             return;
         }
     }
+
+    /* In VSC825X PHYs, by default the GPIO Pins 34, 35, 36, 37 are used for LINE side LOPC signal but as per the EVB Hardware,
+     * these signals are not handled, so disable the LOPC detection from GPIO PIN and invert the coresponding GPIO PIN to High state
+     * The LOPC register of each channel should be configured with 0x13F, where the base address for channel 0 register is 0xf234
+     */
+    uint32_t lopc_reg_addr = 0xf234 + (gmap->channel_id - 1);
+    uint32_t reg_mmd = 0x1e;
+    vtss_phy_10g_csr_write(PHY_INST, iport, reg_mmd, lopc_reg_addr, 0x13f);
 }

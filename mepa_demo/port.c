583845826564f34fae15118a337e6dbd85b89710
Horatiu Vultur <horatiu.vultur@microchip.com>
2026-01-26 15:17:00 +0100

    MEPA-XXXX: demo: port: Fix configuration of the speed
    When using mepa_demo I was trying to configure the port at different
    speeds and then I have noticed that the traffic stop being switch.
    This issue was seen on eds2 board when no daughter cards were plug in.
    To reproduce the issue the following commands were run:
    
    mepa-cmd port mode 1 100fdx
    mepa-cmd port mode 1 auto
    
    Then you could see that no traffic was forward anymore between the
    ports. The mepa demo was waiting for the PHY to get a link and it was
    sucessfully auto neg to 1G as expected by when it was configuring the
    MAC side it was still setting the speed 100. Apparently we configure the
    speed to previous configured speed which is wrong.
    Once this line is removed then things are working as expected. I can see
    that this line was introduced to fix MEPA-621 but that is not the
    correct behaviour.

--- mepa_demo/port.c
+++ mepa_demo/port.c
@@ -388,7 +388,6 @@ static void port_setup(mesa_port_no_t port_no, mesa_bool_t aneg, mesa_bool_t ini
             conf.speed = pc->speed;
         }
     }
-    conf.speed = pc->speed; // Update the speed always irespective of the media type
     if (port_no == loop_port) { // This port is the active loop port
         conf.loop = 1;
     }
